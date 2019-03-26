f63080dbb94c7fb7a9489606a0f6b6af2cdb903f
Mishael Trash <pro100trash@gmail.com>
2019-03-26 21:53:51 +0300

    Repair structure and add gradle`s files

---
+++ src/main/java/com/starlabs/RadioGaGa/RadioGaGaApplication.java
@@ -0,0 +1,13 @@
+package com.starlabs.RadioGaGa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioGaGaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioGaGaApplication.class, args);
+	}
+
+}
